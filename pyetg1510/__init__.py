0f29155848a482d12ca6508cfda24f7327fdb302
Takashi Ichihashi <t.ichihashi@bekchoff.co.jp>
2023-10-24 13:49:40 +0900

    Fix publish_dir of github pages

--- pyetg1510/__init__.py
+++ pyetg1510/__init__.py
@@ -1,11 +1,11 @@
 from .helper.logging_service import *
 from .helper.settings import *
-from .sdo_fxxx_controls import *
 from .etg_1510 import *
 from .sdo_1xxx_master_object import *
 from .sdo_8xxx_configuration_data import *
 from .sdo_9xxx_information_data import *
 from .sdo_axxx_master_diagnosis import *
+from .sdo_fxxx_controls import *
 
 VERSION = (0, 0, 1)
 
